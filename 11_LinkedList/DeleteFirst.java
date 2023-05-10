8f50f026c1e15c46a1ca2732b1e013f46f3e0010
IbrahimGhasia <ibrahim.ghasia@gmail.com>
2023-05-10 23:40:51 +0530

    10th May,23

---
+++ 11_LinkedList/DeleteFirst.java
@@ -0,0 +1,40 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class DeleteFirst {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        System.out.println("Before: ");
+        printList(head);
+        System.out.println("\nAfter: ");
+        head = deleteFirst(head);
+        printList(head);
+    }
+
+    public static Node deleteFirst(Node head) {
+        if (head == null)
+            return null;
+        else
+            return head.next;
+    }
+
+    public static void printList(Node head) {
+        Node curr = head;
+
+        while (curr != null) {
+            System.out.print("->" + curr.data);
+            curr = curr.next;
+        }
+    }
+
+}
---
+++ 11_LinkedList/DeleteLast.java
@@ -0,0 +1,22 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class DeleteLast {
+    public static void main(String[] args) {
+
+    }
+
+    public static Node deleteLast(Node head) {
+        Node temp = head.next;
+        while (head.next != null) {
+
+        }
+    }
+}
